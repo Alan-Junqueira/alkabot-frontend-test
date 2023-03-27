0f7d912b4dc3a0905f626eaa733f90255e4b0013
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-03-26 23:48:35 -0300

    feat: create UserInfo skeleton

---
+++ src/app/components/skeletion/UserInfoLoading.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const UserInfoLoading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-4 bg-gradient-to-r from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25">
+      <div className="mb-3 rounded-full h-32 w-32 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+      <div className="my-1 h-7 w-1/5 bg-gradient-to-l from-gray-200 to-gray-300 animate-pulse bg-gray-400 bg-opacity-25"></div>
+    </div>
+  )
+}
--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -2,8 +2,11 @@ import { Post as PostType } from '@/app/@types/post'
 import { User as UserType } from '@/app/@types/user'
 import { Post } from '@/app/components/Post'
 import { UserInfo } from '@/app/components/UserInfo'
+import { PostLoading } from '@/app/components/skeletion/PostLoading'
+import { UserInfoLoading } from '@/app/components/skeletion/UserInfoLoading'
 import { axiosInstance } from '@/app/services/axiosInstance'
 import { notFound } from 'next/navigation'
+import { Suspense } from 'react'
 
 type UserProps = {
   params: {
@@ -48,16 +51,22 @@ export default async function User({ params: { id } }: UserProps) {
 
   return (
     <>
-      <UserInfo user={userInfo.data} />
+      <Suspense fallback={<UserInfoLoading />}>
+        <UserInfo user={userInfo.data} />
+      </Suspense>
       <h2 className="text-center bg-purple-800 mt-8 py-4 text-gray-100 border-2 border-gray-100 border-solid text-xl">
         {user.name} Posts
       </h2>
       <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4 sm:px-4 lg:px-8">
         {userPostsResult.map((post) => (
-          <div key={post.id} className="mt-4">
-            {/* @ts-expect-error  Acync Server Component */}
-            <Post body={post.body} postId={post.id} title={post.title} />
-          </div>
+          <>
+            <Suspense fallback={<PostLoading />}>
+              <div key={post.id} className="mt-4">
+                {/* @ts-expect-error  Acync Server Component */}
+                <Post body={post.body} postId={post.id} title={post.title} />
+              </div>
+            </Suspense>
+          </>
         ))}
       </div>
     </>
